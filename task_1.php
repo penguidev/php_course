1c96c2e8a0a14f1faffbbf154609a010791301bc
Reshat <reschtmus@gmail.com>
2021-12-13 17:58:33 +0500

    1 task done

--- task_1.php
+++ task_1.php
@@ -43,20 +43,20 @@
                                             </div>
                                         </div>
                                     </div>
+
+                                    <?php 
+                                        $titles = ['One', 'Two', 'Three', 'Four', 'Five']
+                                    ?>
+
                                     <ul id="js-list-msg" class="list-group px-2 pb-2 js-list-filter">
+                                        <?php foreach($titles as $title): ?>
                                         <li class="list-group-item">
-                                            <span data-filter-tags="reports file">Reports</span>
+                                            <span data-filter-tags="reports file"><?php echo $title ?></span>
                                         </li>
-                                        <li class="list-group-item">
-                                            <span data-filter-tags="analytics graphs">Analytics</span>
-                                        </li>
-                                        <li class="list-group-item">
-                                            <span data-filter-tags="export download">Export</span>
-                                        </li>
-                                        <li class="list-group-item">
-                                            <span data-filter-tags="storage">Storage</span>
-                                        </li>
+                                        <?php endforeach; ?>
                                     </ul>
+
+
                                     <div class="filter-message js-filter-message mt-0 fs-sm"></div>
                                 </div>
                             </div>
